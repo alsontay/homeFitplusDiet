0c26ba4a0cb3e16523c350d9dc46cc414fc08862
jiaqi14 <71432005+jiaqi14@users.noreply.github.com>
2020-10-24 15:44:51 +0800

    Initialize project with vuerouter, vuematerial, prettier, husky (#1)
    * Some reorganisation of components
    
    * Added Firebase stuff and api test
    
    * Set up vue router and vue material
    
    * Added prettier and husky
    
    Co-authored-by: Alson Tay Zhi Sheng <66072780+alsontay@users.noreply.github.com>
    Co-authored-by: llama-cookies <iamlcaf1@gmail.com>
    Co-authored-by: alsontay <alsonlgkai@hotmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <div id="app">
     <Header />
-
-
-    <div id = "body">
-      <h2>WELCOME TO HOMEFIT+DIET!</h2>
-      <h3>We aim to help you achieve your goals in fitness and dieting while you are stuck at home!</h3>
-      <h3>Our fitness and diet plans are crafted by an algorithm designed by professional coaches and nutritionists.</h3>
-      <h3>So if you stuck to the plans, we guarantee that you will see results!</h3>
-      <br/>
-      <h3>Happy EATING FIT and KEEPING FIT!</h3>
+    <!-- <nav>
+      Home  {{'\xa0'}} {{'\xa0'}}
+      Progress
+    </nav> -->
+    <div id="body">
+      <router-view></router-view>
     </div>
   </div>
 </template>
@@ -18,29 +15,28 @@
 import Header from "./components/Header.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Header
-  }
-}
+    Header,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 #body {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: rgb(121, 212, 145);
-  background:rgb(243, 239, 239);
-  padding: 120px;
+  width: 100vw;
+  height: 80vh;
+}
+
+nav {
+  padding: 5px;
+  padding-bottom: 15px;
+  color: #2c3e50;
+  background: rgb(185, 222, 195);
 }
 </style>
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import LandingPage from "./components/LandingPage/LandingPage.vue";
+
+/**
+ * Array of url routes.
+ */
+export const routes = [
+  { path: "/", component: LandingPage },
+  // { path: '/signin', component:  },
+  // { path: '/signup', component:  },
+  // { path: '/home', component:  },
+];
