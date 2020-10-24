6d61cfddcb041a4cacd459f87073931c5bbf5c03
unknown <jiaqi14@gmail.com>
2020-10-25 02:55:28 +0800

    Fixed Login and Signup pages, Added error message for login page

---
+++ src/components/HomePage/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page">
+    HOME PAGE
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style scoped>
+@import "./HomePage.css";
+</style>
