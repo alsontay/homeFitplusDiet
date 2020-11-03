ebcc7f2dac5e6dff63649d02c87739a872905aea
llama-cookies <iamlcaf1@gmail.com>
2020-11-03 21:39:05 +0800

    Inital Chart Preparation
    - added charts to monthly report page
    - but it's still pretty buggy rn

--- src/components/MonthlyReportPage.vue
+++ src/components/MonthlyReportPage.vue
@@ -1,11 +1,72 @@
 <template>
-  <div>Hi, This is the monthly report page.</div>
+  <div>
+    <mdb-row id="title-text">
+        <h2> Monthly Report </h2>
+        <h3> Hello {{this.user.data.displayName}}, here's your progress over the past 30 entries!</h3>
+    </mdb-row>
+    <mdb-container>
+      <mdb-row>
+        <div id="chart-bg-1">
+        <mdb-col>
+          <h4> Calorie Consumption </h4>
+          <calorie-chart> </calorie-chart>
+        </mdb-col>
+        </div>
+        <mdb-col>
+          <h4> Second Chart </h4>
+        </mdb-col>
+      </mdb-row>
+    </mdb-container>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import CalorieChart from "./charts/CalorieChart.vue";
+
 export default {
   name: "MonthlyReportPage",
+  components: {
+    CalorieChart,
+  },
+  computed: {
+    ...mapGetters({
+      user: "user",
+    }),
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#title-text {
+  padding: 20px;
+  padding-bottom: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h2 {
+  font-family: "Roboto";
+  font-size: 35px;
+  font-weight: 500;
+}
+
+h3 {
+  font-family: "Roboto";
+  font-size: 25px;
+  font-weight: 250;
+}
+
+h4 {
+  font-family: "Roboto";
+  font-size: 18px;
+  font-weight: 250;
+}
+
+#chart-bg-1 {
+  padding: 15px;
+  background-color: "grey";
+}
+</style>
