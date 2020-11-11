9dc3ed8dc6d999d89ad75e618dcd1819ee7d0129
jiaqi14 <71432005+jiaqi14@users.noreply.github.com>
2020-11-12 01:09:16 +0800

    autocomplete form completed, changed ingredients csv to ingredients json

--- src/components/MenuPage.vue
+++ src/components/MenuPage.vue
@@ -1,161 +1,162 @@
 <template>
-	<div class="autocomplete">
-		<input
-			type="text"
-			@input="onChange"
-			v-model="search"
-			@keydown.down="onArrowDown"
-			@keydown.up="onArrowUp"
-			@keydown.enter="onEnter"
-		/>
-		<ul id="autocomplete-results" v-show="isOpen" class="autocomplete-results">
-			<li class="loading" v-if="isLoading">Loading results...</li>
-			<li
-				v-else
-				v-for="(result, i) in results"
-				:key="i"
-				@click="setResult(result)"
-				class="autocomplete-result"
-				:class="{ 'is-active': i === arrowCounter }"
-			>
-				{{ result }}
-			</li>
-		</ul>
-	</div>
+  <div class="autocomplete">
+    
+      <input id = "textbox"
+        type="text"
+        @input="onChange"
+        v-model="search"
+        @keydown.down="onArrowDown"
+        @keydown.up="onArrowUp"
+        @keydown.enter="onEnter"
+        autocomplete="off"
+      />
+      <ul id="autocomplete-results" v-show="isOpen" class="autocomplete-results">
+        <li class="loading" v-if="isLoading">Loading results...</li>
+        <li
+          v-else
+          v-for="(result, i) in results"
+          :key="i"
+          @click="setResult(result)"
+          class="autocomplete-result"
+          :class="{ 'is-active': i === arrowCounter }"
+        >
+          {{ result }}
+        </li>
+      </ul>
+      <mdb-btn type="submit" @click="onSubmit" color="default">Submit</mdb-btn>
+      <br>
+      {{selectedIngred}}
+    
+  </div>
 </template>
 
 <script>
 // import Recipes from "./APIs/recipes.vue";
-// import ingredients from "../assets/top-1k-ingredients.csv";
-const fs = require("fs");
-import * as csv from "csv-parser";
+import ingredients from "../assets/new_ingredients.json";
+
 
 export default {
   name: "MenuPage",
   // components: {
   //   "app-recipes": Recipes
   // },
-  props: {
-      items: {
-        type: Array,
-        required: false,
-        default: () => {
-          const ingred = [];
-          fs.createReadStream("../assets/top-1k-ingredients.csv").pipe(csv(({separator:';'})))
-          .on('data', (data) => ingred.push(data))
-          .on('end', ()=> ingred
-          );
-          return ingred;
-        },
-      },
-      isAsync: {
-        type: Boolean,
-        required: false,
-        default: false,
-      },
-    },
 
-    data() {
-      return {
-        isOpen: false,
-        results: [],
-        search: '',
-        isLoading: false,
-        arrowCounter: 0,
-      };
-    },
 
-    methods: {
-      onChange() {
+  data() {
+    return {
+      isOpen: false,
+      results: [],
+      search: "",
+      isLoading: false,
+      arrowCounter: 0,
+      items: ingredients,
+      selectedIngred: [],
+    };
+  },
 
-        this.$emit('input', this.search);
+  methods: {
+    onChange() {
+      this.$emit("input", this.search);
 
-        if (this.isAsync) {
-          this.isLoading = true;
-        } else {
-          // Let's  our flat array
-          this.filterResults();
-          this.isOpen = true;
-        }
-      },
-
-      filterResults() {
-        // first uncapitalize all the things
-        this.results = this.items.filter((item) => {
-          return item.toLowerCase().indexOf(this.search.toLowerCase()) > -1;
-        });
-      },
-      setResult(result) {
-        this.search = result;
-        this.isOpen = false;
-      },
-      onArrowDown(evt) {
-        if (this.arrowCounter < this.results.length) {
-          this.arrowCounter = this.arrowCounter + 1;
-        }
-      },
-      onArrowUp() {
-        if (this.arrowCounter > 0) {
-          this.arrowCounter = this.arrowCounter -1;
-        }
-      },
-      onEnter() {
-        this.search = this.results[this.arrowCounter];
-        this.isOpen = false;
-        this.arrowCounter = -1;
-      },
-      handleClickOutside(evt) {
-        if (!this.$el.contains(evt.target)) {
-          this.isOpen = false;
-          this.arrowCounter = -1;
-        }
+      if (this.isAsync) {
+        this.isLoading = true;
+      } else {
+        // Let's  our flat array
+        this.filterResults();
+        this.isOpen = true;
       }
     },
-    watch: {
-      items: function (val, oldValue) {
-        // actually compare them
-        if (val.length !== oldValue.length) {
-          this.results = val;
-          this.isLoading = false;
-        }
-      },
+
+    filterResults() {
+      // first uncapitalize all the things
+      this.results = this.items.filter((item) => {
+        return item.toLowerCase().indexOf(this.search.toLowerCase()) > -1;
+      });
     },
-
-    mounted() {
-      document.addEventListener('click', this.handleClickOutside)
+    setResult(result) {
+      this.search = result;
+      this.isOpen = false;
     },
-    destroyed() {
-      document.removeEventListener('click', this.handleClickOutside)
-    }
-  };
+    onArrowDown(evt) {
+      if (this.arrowCounter < this.results.length) {
+        this.arrowCounter = this.arrowCounter + 1;
+      }
+    },
+    onArrowUp() {
+      if (this.arrowCounter > 0) {
+        this.arrowCounter = this.arrowCounter - 1;
+      }
+    },
+    onEnter() {
+      // if not pointing to autocomplete, don't do anything to search input
+      if (! this.results[this.arrowCounter]) return;
+      this.search = this.results[this.arrowCounter];
+      this.isOpen = false;
+      this.arrowCounter = -1;
+    },
+    onSubmit() {
+      // event.preventDefault();
+      this.selectedIngred.push(this.search);
+      this.search = "";
+      console.log(this.selectedIngred);
+    },
+    handleClickOutside(evt) {
+      if (!this.$el.contains(evt.target)) {
+        this.isOpen = false;
+        this.arrowCounter = -1;
+      }
+    },
+  },
+  watch: {
+    items: function (val, oldValue) {
+      if (val.length !== oldValue.length) {
+        this.results = val;
+        this.isLoading = false;
+      }
+    },
+  },
 
 
+
+  mounted() {
+    document.addEventListener("click", this.handleClickOutside);
+  },
+  destroyed() {
+    document.removeEventListener("click", this.handleClickOutside);
+  },
+};
 </script>
 
 <style scoped>
+#textbox{
+  padding: 10px;
+  margin-top: 20px;
+  height:50px;
+  width: 300px;
+}
 .autocomplete {
-	position: relative;
+  position: relative;
 }
 
 .autocomplete-results {
-	padding: 0;
-	margin: 0;
-	border: 1px solid #eeeeee;
-	height: 120px;
-	overflow: auto;
-	width: 100%;
+  padding: 0;
+  margin: 0;
+  border: 1px solid #eeeeee;
+  height: 120px;
+  overflow: auto;
+  
 }
 
 .autocomplete-result {
-	list-style: none;
-	text-align: left;
-	padding: 4px 2px;
-	cursor: pointer;
+  list-style: none;
+  text-align: left;
+  padding: 4px 2px;
+  cursor: pointer;
 }
 
 .autocomplete-result.is-active,
 .autocomplete-result:hover {
-	background-color: #4aae9b;
-	color: white;
+  background-color: #4aae9b;
+  color: white;
 }
 </style>
--- src/components/autocomplete.vue
+++ src/components/autocomplete.vue
@@ -1,93 +1,93 @@
 <script>
-  export default {
-    name: 'autocomplete',
+export default {
+  name: "autocomplete",
 
-    props: {
-      items: {
-        type: Array,
-        required: false,
-        default: () => [],
-      },
-      isAsync: {
-        type: Boolean,
-        required: false,
-        default: false,
-      },
+  props: {
+    items: {
+      type: Array,
+      required: false,
+      default: () => [],
     },
-
-    data() {
-      return {
-        isOpen: false,
-        results: [],
-        search: '',
-        isLoading: false,
-        arrowCounter: 0,
-      };
+    isAsync: {
+      type: Boolean,
+      required: false,
+      default: false,
     },
+  },
 
-    methods: {
-      onChange() {
-        // Let's warn the parent that a change was made
-        this.$emit('input', this.search);
+  data() {
+    return {
+      isOpen: false,
+      results: [],
+      search: "",
+      isLoading: false,
+      arrowCounter: 0,
+    };
+  },
 
-        // Is the data given by an outside ajax request?
-        if (this.isAsync) {
-          this.isLoading = true;
-        } else {
-          // Let's  our flat array
-          this.filterResults();
-          this.isOpen = true;
-        }
-      },
+  methods: {
+    onChange() {
+      // Let's warn the parent that a change was made
+      this.$emit("input", this.search);
 
-      filterResults() {
-        // first uncapitalize all the things
-        this.results = this.items.filter((item) => {
-          return item.toLowerCase().indexOf(this.search.toLowerCase()) > -1;
-        });
-      },
-      setResult(result) {
-        this.search = result;
-        this.isOpen = false;
-      },
-      onArrowDown(evt) {
-        if (this.arrowCounter < this.results.length) {
-          this.arrowCounter = this.arrowCounter + 1;
-        }
-      },
-      onArrowUp() {
-        if (this.arrowCounter > 0) {
-          this.arrowCounter = this.arrowCounter -1;
-        }
-      },
-      onEnter() {
-        this.search = this.results[this.arrowCounter];
-        this.isOpen = false;
-        this.arrowCounter = -1;
-      },
-      handleClickOutside(evt) {
-        if (!this.$el.contains(evt.target)) {
-          this.isOpen = false;
-          this.arrowCounter = -1;
-        }
+      // Is the data given by an outside ajax request?
+      if (this.isAsync) {
+        this.isLoading = true;
+      } else {
+        // Let's  our flat array
+        this.filterResults();
+        this.isOpen = true;
       }
     },
-    watch: {
-      items: function (val, oldValue) {
-        // actually compare them
-        if (val.length !== oldValue.length) {
-          this.results = val;
-          this.isLoading = false;
-        }
-      },
+
+    filterResults() {
+      // first uncapitalize all the things
+      this.results = this.items.filter((item) => {
+        return item.toLowerCase().indexOf(this.search.toLowerCase()) > -1;
+      });
     },
-    mounted() {
-      document.addEventListener('click', this.handleClickOutside)
+    setResult(result) {
+      this.search = result;
+      this.isOpen = false;
     },
-    destroyed() {
-      document.removeEventListener('click', this.handleClickOutside)
-    }
-  };
+    onArrowDown(evt) {
+      if (this.arrowCounter < this.results.length) {
+        this.arrowCounter = this.arrowCounter + 1;
+      }
+    },
+    onArrowUp() {
+      if (this.arrowCounter > 0) {
+        this.arrowCounter = this.arrowCounter - 1;
+      }
+    },
+    onEnter() {
+      this.search = this.results[this.arrowCounter];
+      this.isOpen = false;
+      this.arrowCounter = -1;
+    },
+    handleClickOutside(evt) {
+      if (!this.$el.contains(evt.target)) {
+        this.isOpen = false;
+        this.arrowCounter = -1;
+      }
+    },
+  },
+  watch: {
+    items: function (val, oldValue) {
+      // actually compare them
+      if (val.length !== oldValue.length) {
+        this.results = val;
+        this.isLoading = false;
+      }
+    },
+  },
+  mounted() {
+    document.addEventListener("click", this.handleClickOutside);
+  },
+  destroyed() {
+    document.removeEventListener("click", this.handleClickOutside);
+  },
+};
 </script>
 
 <template>
@@ -100,17 +100,8 @@
       @keydown.up="onArrowUp"
       @keydown.enter="onEnter"
     />
-    <ul
-      id="autocomplete-results"
-      v-show="isOpen"
-      class="autocomplete-results"
-    >
-      <li
-        class="loading"
-        v-if="isLoading"
-      >
-        Loading results...
-      </li>
+    <ul id="autocomplete-results" v-show="isOpen" class="autocomplete-results">
+      <li class="loading" v-if="isLoading">Loading results...</li>
       <li
         v-else
         v-for="(result, i) in results"
@@ -126,30 +117,29 @@
 </template>
 
 <style>
-  .autocomplete {
-    position: relative;
-  }
+.autocomplete {
+  position: relative;
+}
 
-  .autocomplete-results {
-    padding: 0;
-    margin: 0;
-    border: 1px solid #eeeeee;
-    height: 120px;
-    overflow: auto;
-    width: 100%;
-  }
+.autocomplete-results {
+  padding: 0;
+  margin: 0;
+  border: 1px solid #eeeeee;
+  height: 120px;
+  overflow: auto;
+  width: 100%;
+}
 
-  .autocomplete-result {
-    list-style: none;
-    text-align: left;
-    padding: 4px 2px;
-    cursor: pointer;
-  }
+.autocomplete-result {
+  list-style: none;
+  text-align: left;
+  padding: 4px 2px;
+  cursor: pointer;
+}
 
-  .autocomplete-result.is-active,
-  .autocomplete-result:hover {
-    background-color: #4AAE9B;
-    color: white;
-  }
-
+.autocomplete-result.is-active,
+.autocomplete-result:hover {
+  background-color: #4aae9b;
+  color: white;
+}
 </style>
