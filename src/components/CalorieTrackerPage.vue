cf2d3ed07030b003c4f6b597c4a6c7ef26260473
llama-cookies <iamlcaf1@gmail.com>
2020-11-13 14:30:16 +0800

    Update CalorieTrackerPage.vue
    Slight bux fix

--- src/components/CalorieTrackerPage.vue
+++ src/components/CalorieTrackerPage.vue
@@ -78,8 +78,9 @@ export default {
       if (db2.exists) {
         db.update(update);
       } else {
-        db.set({ $today: { consume: this.calcount.data.values } });
-        //db.consume.set(this.calcount.data.values);
+        var update2 = {}
+        update2[`${this.calcount.data.date}`] = {consume: this.caclcount.data.values};
+        db.set(update);
       }
       alert("Calorie Counts Updated!");
     },
